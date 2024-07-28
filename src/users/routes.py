348ec24b651613b53e99fdab09230eebc97e4377
Revan190 <programmer23w@gmail.com>
2024-07-28 16:04:26 +0200

    Homework_part5

--- src/users/routes.py
+++ src/users/routes.py
@@ -1,4 +1,17 @@
-from src.common.routes import BaseCrudPrefixes
+from typing import Annotated, Union
+
+from fastapi import APIRouter, Depends, status
+
+from src.authentication.utils import get_current_user
+from src.common.schemas.common import ErrorResponse
+from src.users.models.user import User
+
+class BaseCrudPrefixes:
+    root: str = '/'
+    create: str = '/create'
+    read: str = '/read'
+    update: str = '/update'
+    delete: str = '/delete'
 
 class UserManagementRoutesPrefixes:
     user: str = '/user'
@@ -9,5 +22,28 @@ class UserRoutesPrefixes(BaseCrudPrefixes):
         self.user_management = UserManagementRoutesPrefixes()
 
 user_routes = UserRoutesPrefixes()
+
+router = APIRouter(prefix=user_routes.user_management.user)
+
+@router.get(
+    user_routes.root,
+    responses={
+        status.HTTP_200_OK: {'model': User},
+        status.HTTP_404_NOT_FOUND: {'model': ErrorResponse},
+    },
+    status_code=status.HTTP_200_OK,
+    response_model=Union[User, ErrorResponse],
+)
+async def user_detail(
+    current_user: Annotated[User, Depends(get_current_user)],
+) -> Union[User, ErrorResponse]:
+    """
+    Retrieve user.
+
+    Returns:
+        Response with user details.
+    """
+    return current_user
+
 print(user_routes.create)
 print(user_routes.user_management.user)
