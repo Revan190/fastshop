657adcc297e449e56bc21a36304e4f0dcd754c03
Revan190 <programmer23w@gmail.com>
2024-08-01 21:20:35 +0200

    Sqlhomework_part1

--- src/basket/utils.py
+++
@@ -1,6 +0,0 @@
-def calculate_total_price(items):
-    total = sum(item.price * item.quantity for item in items)
-    return total
-
-def format_currency(value):
-    return "${:,.2f}".format(value)
