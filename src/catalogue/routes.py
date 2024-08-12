564bb827bf552e1239646c76f185891ba9b5009b
Revan190 <programmer23w@gmail.com>
2024-08-12 17:44:16 +0200

    Homework_part1_views1

--- src/catalogue/routes.py
+++ src/catalogue/routes.py
@@ -1,8 +1,8 @@
 from src.common.routes import BaseCrudPrefixes
 
-
 class CatalogueRoutesPrefixes:
     product: str = '/product'
+    category: str = '/category'
 
 
 class ProductRoutesPrefixes(BaseCrudPrefixes):
